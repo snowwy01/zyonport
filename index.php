ec69e316335be62b3286f7cabb3a2e98dd7c6705
snowwy01 <516749@vistacollege.nl>
2024-05-29 14:18:37 +0200

    alles

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+<div>
+    <button class="knop1"><a href="over_mij.php" style="text-decoration: none; color: white;">over mij</a></button>
+    <br><br><br>
+    <button class="knop1"><a href="projecten.php" style="text-decoration: none; color: white;">projecten</a></button>
+<br><br><br>
+<button class="knop1"><a href="progressie.php" style="text-decoration: none; color: white;">progressie</a></button>
+<br><br><br>
+<button class="knop1"><a href="stage.php" style="text-decoration: none; color: white;">stage</a></button>
+<br><br><br>
+<button class="homeknop"><a href="index.php" style="text-decoration: none; color: white;">Home</a></button>
+</div>                  
+
+<div class="logo">
+    <img src="port-removebg-preview.png" alt="Logo" style="width:400px; height:400px;">
+</div>
+
 </body>
 </html>
